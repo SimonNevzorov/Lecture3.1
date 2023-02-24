c054dce987659c605601ebabbe4535a5f9a34c68
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-02-24 23:46:23 +0300

    Example007-Base

---
+++ Example007-Magic/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 15;
+int yb = 25;
+int xc = 60; 
+int yc = 30;
+     
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+//Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
+
